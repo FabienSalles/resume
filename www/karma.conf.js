6b314aa8a74c1787147f7e8a99cc60aacbacd66a
Fabien Salles <blacked19@gmail.com>
2014-04-03 15:08:55 +0200

    update packages

--- www/karma.conf.js
+++ www/karma.conf.js
@@ -4,7 +4,7 @@
 module.exports = function(config) {
   config.set({
     // base path, that will be used to resolve files and exclude
-    basePath: '',
+    basePath: './',
 
     // testing framework to use (jasmine/mocha/qunit/...)
     frameworks: ['jasmine'],
@@ -29,9 +29,11 @@ module.exports = function(config) {
     // web server port
     port: 8080,
 
+    colors: true,
+
     // level of logging
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_DEBUG,
 
 
     // enable / disable watching file and executing tests whenever any file changes
@@ -58,11 +60,13 @@ module.exports = function(config) {
       'karma-coverage',
       'karma-phantomjs-launcher'
     ],
+
     reporters: ['progress', 'coverage'],
+
     preprocessors: { 'app/scripts/**/*.js': ['coverage'] },
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
-    singleRun: true
+    singleRun: false
   });
 };
