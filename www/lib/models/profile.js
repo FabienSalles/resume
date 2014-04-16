2ba800fe2c97ad19bce8467ffeb8694263e595c7
Fabien Salles <blacked19@gmail.com>
2014-04-16 19:41:08 +0200

    initialize profile model and api rest for the profile #6

---
+++ www/lib/models/profile.js
@@ -0,0 +1,66 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    uniqueValidator = require('mongoose-unique-validator'),
+    Schema = mongoose.Schema;
+
+/**
+ * Profile Schema
+ */
+var ProfileSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  phoneNumber: String,
+  jobName: String,
+  description: String,
+  age: { type: Number, min: 18, max: 65 },
+  email: { type: String, unique: true },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+
+/**
+ * Basic information of Profile
+ */
+ProfileSchema
+  .virtual('ProfileInfo')
+  .get(function() {
+    return {
+      'firstName': this.firstName,
+      'lastName': this.lastName,
+      'phoneNumber': this.phoneNumber,
+      'jobName' : this.jobName,
+      'description' : this.description,
+      'age' : this.age,
+      'email' : this.email
+    };
+  });
+
+
+/**
+ * Validations
+ */
+var validatePresenceOf = function(value) {
+  return value && value.length;
+};
+
+
+/**
+ * Plugins
+ */
+ProfileSchema.plugin(uniqueValidator,  { message: 'Value is not unique.' });
+
+/**
+ * Pre-save hook
+ */
+ProfileSchema
+  .pre('save', function(next) {
+    if (!this.isNew) return next();
+
+    this.updatedAt = Date.now;
+    next();
+  });
+
+
+mongoose.model('Profile', ProfileSchema);
--- www/lib/routes.js
+++ www/lib/routes.js
@@ -3,8 +3,9 @@
 var api = require('./controllers/api'),
     index = require('./controllers'),
     users = require('./controllers/users'),
+    profiles = require('./controllers/profiles'),
     session = require('./controllers/session');
-    
+
 var middleware = require('./middleware');
 
 /**
@@ -14,11 +15,16 @@ module.exports = function(app) {
 
   // Server API Routes
   app.get('/api/awesomeThings', api.awesomeThings);
-  
+
   app.post('/api/users', users.create);
   app.put('/api/users', users.changePassword);
   app.get('/api/users/me', users.me);
   app.get('/api/users/:id', users.show);
+
+  app.post('/api/profiles', profiles.create);
+  app.get('/api/profiles/me', profiles.me);
+  app.get('/api/profiles/:id', profiles.show);
+  app.put('/api/profiles/:id', profiles.update);
 
   app.post('/api/session', session.login);
   app.del('/api/session', session.logout);
