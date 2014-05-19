7f6002d9a4dbb5aa0f48459fb7a00e317afb80c8
Fabien Salles <blacked19@gmail.com>
2014-05-19 09:17:19 +0200

    #6 - add unit test for profile

--- www/spec/lib/model/profileSpec.js
+++ www/spec/lib/model/profileSpec.js
@@ -15,6 +15,27 @@ describe("Profile", function () {
     expect(newProfile.createdAt.toUTCString()).toEqual(now.toUTCString());
     expect(newProfile.updatedAt.toUTCString()).toEqual(now.toUTCString());
   });
+
+  it("should return profile information", function () {
+    var newProfile = Profile.model({
+      firstName : "Bob",
+      lastName : "Henry",
+      phoneNumber : "0555555555",
+      jobName : "developer",
+      description : "I want a job",
+      age : 23,
+      email : "bob@henry.com"
+    });
+
+    var profileInfo = newProfile.get('ProfileInfo');
+    expect(profileInfo.firstName).toEqual("Bob");
+    expect(profileInfo.lastName).toEqual("Henry");
+    expect(profileInfo.phoneNumber).toEqual("0555555555");
+    expect(profileInfo.jobName).toEqual("developer");
+    expect(profileInfo.description).toEqual("I want a job");
+    expect(profileInfo.age).toEqual(23);
+    expect(profileInfo.email).toEqual("bob@henry.com");
+  });
 });
 
 mongoose.connection.close();
