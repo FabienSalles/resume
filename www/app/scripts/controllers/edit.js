f05631bc114bb254b1fa276abd7083d0f6ca3e4f
Yves Renaud <ys.renaud@gmail.com>
2014-05-23 17:14:11 +0200

    #3 - Intégration du bloc profil, experiences et diplomes

--- www/app/scripts/controllers/edit.js
+++ www/app/scripts/controllers/edit.js
@@ -4,16 +4,28 @@ angular.module('wwwApp')
 .controller('EditCtrl', function ($scope, $http, $location) {
 
 
-	$scope.user = {};
-	$scope.errorMessage = '';
-
-	$scope.skills = [
-	    {
+	$scope.user = {
+		name: 'Renaud',
+		firstname: 'Yves',
+		profil: 'Je suis motivé!',
+		experience: 'Je suis un expert!',
+		skills: [{
 	    	id: 1, name: 'PHP', desc: 'Mes compétences en PHP'
 	    },{
 	    	id: 2, name: 'HTML', desc: 'Mes compétences en HTML'
-	    }
-  	]; 
+	    }],
+	    diplomes: [{
+	    	id: 1,
+	    	name: "BAC STI",
+	    	desc: "J'ai passé mon bac au lycée."
+	    }]
+	};
+
+
+
+	$scope.errorMessage = '';
+
+
 
 	// A deplacer dans un model User
 	$scope.updateName = function(data) {
@@ -50,10 +62,19 @@ angular.module('wwwApp')
   	// Ajoute un champ pour saisir une compétence
 	$scope.addSkill = function() {
 		$scope.inserted = {
-		  id: $scope.skills.length + 1,
+		  id: $scope.user.skills.length + 1,
 		  name: ''
 		};
-		$scope.skills.push($scope.inserted);
+		$scope.user.skills.push($scope.inserted);
+	};
+
+	// Ajoute un champ pour saisir un diplome
+	$scope.addDiplome = function() {
+		$scope.inserted = {
+		  id: $scope.user.diplomes.length + 1,
+		  name: ''
+		};
+		$scope.user.diplomes.push($scope.inserted);
 	};
 
 });
